43996853d5b390ed9a5ab0da67b1fed55cb957ed
benMen87 <sretrh@gmail.com>
2018-06-12 21:56:12 +0300

    adding initial model with logic for splitting mnist to unlabeld and labeld in a blanced way

---
+++ common.py
@@ -0,0 +1,174 @@
+from __future__ import division
+import os
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import numpy as np
+
+
+def to_np(_x): return _x.data.cpu().numpy()
+
+def I(_x): return _x
+
+def normilize(_x, _val=255): return _x / _val
+
+def nhwc_to_nchw(_x, keep_dims=True): 
+    if len(_x.shape) == 3 and (_x.shape[-1] == 1 or _x.shape[-1] == 3): #unsqueeze N dim
+        _x = _x[None, ...]
+    elif len(_x.shape) == 3: #unsqueezed C dim
+        _x = _x[...,None]
+    elif len(_x.shape) == 2:  #unsqueeze N and C dim
+        _x = _x[None,:,:,None]
+    return np.transpose(_x, (0, 3, 1, 2))
+
+def get_unique_name(path):
+    idx = 1
+    _path = path
+    while os.path.isdir(_path):
+        _path = '{}_{}'.format(path, idx)
+        idx += 1
+    return _path
+
+def init_model_dir(path, name):
+    full_path = os.path.join(path, name)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    else:
+        full_path = get_unique_name(full_path)
+    os.mkdir(full_path)
+    os.mkdir(os.path.join(full_path, 'saved'))
+    return full_path, os.path.join(full_path, 'saved')
+
+    '''
+        Either string defining an activation function or module (e.g. nn.ReLU)
+    '''
+    if isinstance(act_fun, str):
+        if act_fun == 'LeakyReLU':
+            return nn.LeakyReLU(0.2, inplace=True)
+        elif act_fun == 'ELU':
+            return nn.ELU()
+        elif act_fun == 'none':
+            return nn.Sequential()
+        else:
+            assert False
+    else:
+        return act_fun()
+
+
+def flip(x, dim):
+    dim = x.dim() + dim if dim < 0 else dim
+    inds = tuple(slice(None, None) if i != dim
+             else x.new(torch.arange(x.size(i)-1, -1, -1).tolist()).long()
+             for i in range(x.dim()))
+    return x[inds]
+
+def bn(num_features):
+    return nn.BatchNorm2d(num_features)
+
+
+def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
+    downsampler = None
+
+    if stride != 1 and downsample_mode != 'stride':
+        if downsample_mode == 'avg':
+            downsampler = nn.AvgPool2d(stride, stride)
+        elif downsample_mode == 'max':
+           downsampler = nn.MaxPool2d(stride, stride)
+        else:
+            assert False
+        stride = 1
+
+    padder = None
+    to_pad = int((kernel_size - 1) / 2)
+    if pad == 'reflection' and False:
+        padder = nn.ReflectionPad2d(to_pad)
+        to_pad = 0
+
+    convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
+
+
+    layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
+    return nn.Sequential(*layers)
+
+def gaussian(ins, is_training, mean, stddev):
+    if is_training:
+   #     noise = Variable(ins.data.new(ins.size()).normal_(mean, stddev))
+        noise = stddev * torch.randn_like(ins) + mean
+        return ins + noise
+    return ins
+
+#TODO(hillel): this is dangrouse NO default factor val!!!
+def reconsturction_loss(ssim_factor=0.2, use_cuda=True):
+    from pytorch_msssim import MSSSIM, SSIM
+
+    msssim = SSIM()#MSSSIM()
+    l1 = nn.L1Loss()
+    if use_cuda:
+        msssim = msssim.cuda()
+        l1 = l1.cuda()
+    if ssim_factor > 0:
+       return  lambda x, xn: (1 - ssim_factor) * l1(x, xn)  + ssim_factor * (1 - msssim(x, xn))
+    else:
+        return lambda x, xn: l1(x, xn)
+
+def psnr(im, recon, verbose=False):
+    im.shape
+    im = np.squeeze(im)
+    recon = np.squeeze(recon)
+    MSE = np.sum((im - recon)**2) / (im.shape[0] * im.shape[1])
+    MAX = np.max(im)
+    PSNR = 10 * np.log10(MAX ** 2 / MSE)
+    if verbose:
+        print('PSNR %f'%PSNR)
+    return PSNR
+
+def clean(save_path, save_count=10):
+    import glob
+
+    l = glob.glob(save_path)
+
+    if len(l) < save_count:
+        return 
+    l.sort(key=os.path.getmtime) 
+    for f in l[:-save_count]:
+        print('removing', f)
+        os.remove(f)
+    
+def save_train(path, model, optimizer, schedular=None, epoch=None):
+    state = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+    #TODO(hillel): fix this so we can save schedular state
+    #if schedular is not None:
+    #    state['schedular'] = schedular.state_dict()
+    if epoch is not None:
+        state['epoch'] = epoch
+    torch.save(state, os.path.join(path, 'epoch_{}'.format(epoch)))
+    return os.path.join(path, 'epoch_{}'.format(epoch))
+
+def load_train(path, model, optimizer, schedular=None):
+    state = torch.load(path)
+    model.load_state_dict(state['model'])
+    if 'optimizer' in state:
+        optimizer.load_state_dict(state['optimizer'])
+    else:
+        print('Optimizer not inilized since no data for it exists in supplied path')
+    if schedular is not None:
+        if 'schedular' in state:
+            schedular.load_state_dict(state['schedular'])
+        else:
+            print('Schedular not inilized since no data for it exists in supplied path')
+    if 'epoch' in state:
+        e = state['epoch']
+    else: 
+        e = 0
+    return e
+
+def save_eval(path, model):
+    torch.save(model.state_dict(), path)
+
+def load_eval(path, model):
+    model.load_state_dict(torch.load(path)['model'])
+    model.eval()
+
---
+++ train.py
@@ -0,0 +1,162 @@
+from __future__ import division
+import os 
+import sys
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+from torch.optim import lr_scheduler
+from convsparse_net import LISTAConvDictADMM
+import common
+from common import save_train, load_train, clean
+from common import gaussian, normilize, nhwc_to_nchw
+from common import reconsturction_loss, init_model_dir
+from datasets import  DatasetFromNPZ
+from torch.utils.data import DataLoader
+import arguments
+import test_denoise
+
+USE_CUDA = torch.cuda.is_available()
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def get_train_valid_loaders(dataset_path, batch_size, noise):
+   
+    def pre_process_fn(_x): return normilize(nhwc_to_nchw(_x), 255) 
+    def input_process_fn(_x): return gaussian(_x, is_training=True, mean=0, stddev=normilize(noise, 255))
+
+    train_loader = DatasetFromNPZ(npz_path=dataset_path,
+                              key='TRAIN', use_cuda=USE_CUDA,
+                              pre_transform=pre_process_fn,
+                              inputs_transform=input_process_fn)
+    train_loader = DataLoader(train_loader, batch_size=batch_size, shuffle=True)
+
+    valid_loader = DatasetFromNPZ(npz_path=dataset_path,
+                              key='VAL', use_cuda=USE_CUDA,
+                              pre_transform=pre_process_fn,
+                              inputs_transform=input_process_fn)
+    valid_loader = DataLoader(valid_loader, batch_size=1, shuffle=True)
+    return train_loader, valid_loader
+
+def step(model, img, img_n, optimizer=None, criterion=None):
+    if optimizer is not None: 
+       optimizer.zero_grad()
+    output = model(img_n)
+    if criterion is not None:
+        loss = criterion(output[..., 3:-3, 3:-3], img[..., 3:-3, 3:-3])
+        if optimizer:
+            loss.backward()
+            optimizer.step()
+        return loss, output.cpu()
+    return float(output)
+
+def maybe_save_model(model, opt, schd, epoch, save_path, curr_val, other_values):
+    path = ''
+    def no_other_values(other_values):
+        return len(other_values) == 0
+    if no_other_values(other_values) or curr_val > min(other_values):
+        print('saving model...')
+        path = save_train(save_path, model, opt, schd, epoch)
+        clean(save_path, save_count=10)
+    return path
+
+def run_valid(model, data_loader, criterion, logdir):
+    loss = 0
+    psnr = 0
+    for img, img_n in data_loader:
+        _loss, _out = step(model, img, img_n, criterion=criterion)
+        loss += float(_loss)
+        psnr += common.psnr(img.data.cpu().numpy(), _out.data.cpu().numpy())
+
+    _, output = step(model, img, img_n, criterion=criterion)
+    np.savez(os.path.join(logdir, 'images'), IN=img.data.cpu().numpy(),
+        OUT=output.data.cpu().numpy(), NOISE=img_n.data.cpu().numpy())
+    return loss / len(data_loader), psnr / len(data_loader)
+
+def train(model, args):
+    
+    optimizer = optim.Adam(model.parameters(), lr=args['learning_rate'])
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', verbose=True)
+    recon_loss = reconsturction_loss(ssim_factor=args['ssim_factor'], use_cuda=True)
+    train_loader, valid_loader = get_train_valid_loaders(args['dataset_path'], args['batch_size'], args['noise'])
+    print(args)
+    if args['load_path'] != '':
+        ld_p = args['load_path']
+        print('loading from %s'%ld_p)
+        load_train(ld_p, model, optimizer, scheduler)        
+        print('Done!')
+        
+    _train_loss = []
+    _valid_loss = []
+    _valid_psnr = []
+    running_loss = 0
+    valid_every = int(0.1 * len(train_loader))
+
+    itr = 0
+    for e in range(args['epoch']):
+        print('Epoch number {}'.format(e))
+        for img, img_n in train_loader:
+            itr += 1
+
+            _loss, _ = step(model, img, img_n, optimizer, recon_loss)
+            running_loss += float(_loss)
+
+            if itr % valid_every == 0:
+                _train_loss.append(running_loss / valid_every)
+                _v_loss, _v_psnr = run_valid(model, valid_loader,
+                        recon_loss,args['save_dir'])
+                scheduler.step(_v_loss)
+                model_path = maybe_save_model(model, optimizer,
+                        scheduler, e, args['save_dir'],
+                        _v_psnr, _valid_psnr)
+                _valid_loss.append(_v_loss)
+                _valid_psnr.append(_v_psnr)
+                print("epoch {} train loss: {} valid loss: {}, valid psnr: {}".format(e,
+                    running_loss / valid_every, _v_loss, _v_psnr))
+                running_loss = 0
+    return model_path, _valid_loss[-1], _valid_psnr[-1]
+
+def build_model(args):
+    model = LISTAConvDictADMM(
+        num_input_channels=args['num_input_channels'],
+        num_output_channels=args['num_output_channels'],
+        kc=args['kc'], 
+        ks=args['ks'],
+        ista_iters=args['ista_iters'],
+        iter_weight_share=args['iter_weight_share'],
+    )
+    if USE_CUDA:
+        model = model.cuda()
+    return model
+
+def main(args_file):
+    args = arguments.load_args(args_file)
+    log_dir, save_dir = init_model_dir(args['train_args']['log_dir'], args['train_args']['name'])
+    arguments.logdictargs(os.path.join(log_dir, 'params.json'), args)
+    args['train_args']['save_dir'] = save_dir
+    args['train_args']['log_dir'] = log_dir
+    model = build_model(args['model_args'])
+    model_path, valid_loss, valid_psnr = train(model, args['train_args'])
+
+    args['test_args']['load_path'] = model_path
+    args['train_args']['final_loss'] = valid_loss
+    args['train_args']['final_psnr'] = valid_psnr
+    arguments.logdictargs(os.path.join(log_dir, 'params.json'), args)
+
+    psnrs, res = test_denoise.test(args['model_args'],
+         model_path,
+         args['train_args']['noise'], 
+         args['test_args']['testset_path']
+         )
+    args['test_args']['final_psnrs'] = psnrs
+    arguments.logdictargs(os.path.join(log_dir, 'params.json'), args)
+    for idx, ims in enumerate(res):
+       test_denoise.plot_res(ims[0], ims[1], ims[2], idx, args['train_args']['log_dir']
+)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arg_file', default='')
+    arg_file = parser.parse_args().arg_file
+
+    main(arg_file)
